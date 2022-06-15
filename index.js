50bb75accf13fb15f87a8271005cfe762b7ca1d8
Gorilla Jones <kgorski90lol@gmail.com>
2022-06-15 18:42:47 -0500

    Added inquirer/variables/model folder containing employee hubs

---
+++ index.js
@@ -0,0 +1,5 @@
+const inquirer = require('inquirer');
+const employee = require('./models/employee');
+const engineer = require('./models/engineer');
+const intern = require('./models/intern');
+const manager = require('./models/manager');
